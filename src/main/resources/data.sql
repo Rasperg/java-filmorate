2bef870cb71d425ed9949744ef769e245c260445
Denis Golev <110325389+Rasperg@users.noreply.github.com>
2023-04-25 20:37:34 +0500

    Ревью 2

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,6 +5,7 @@ MERGE INTO GENRE (genre_id, name) VALUES (4, 'Триллер');
 MERGE INTO GENRE (genre_id, name) VALUES (5, 'Документальный');
 MERGE INTO GENRE (genre_id, name) VALUES (6, 'Боевик');
 
+
 MERGE INTO MPA (id, name) VALUES (1, 'G');
 MERGE INTO MPA (id, name) VALUES (2, 'PG');
 MERGE INTO MPA (id, name) VALUES (3, 'PG-13');
